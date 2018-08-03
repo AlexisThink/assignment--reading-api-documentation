952485d19ddcdbe759330094fc037da9b9af2979
Alexis Olveres <alexis.olv.developer@gmail.com>
2018-08-02 21:31:12 -0500

    Geolocalizacion hecha

--- js/api-03-google-sunrise-darksky.js
+++ js/api-03-google-sunrise-darksky.js
@@ -12,7 +12,7 @@
 
 */
 
-
+var request = superagent;
 //========================================================================
 //  (1) What are latitude longitude coordinates of Montreal?
 //
@@ -21,26 +21,63 @@
 //
 
 const answerElement_apimashup_1 = document.getElementById('apimashup-1')
+var googleAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address=Montreal';
 
 
+request
+  .get(googleAPI)
+  .then(function(response){
 
+    var latitude = response.body.results[0].geometry.location.lat;
+    var longitude = response.body.results[0].geometry.location.lng;
+
+    var resultado = 'Latitud: ' + latitude + ' Longitud: ' + longitude;
+
+    answerElement_apimashup_1.textContent = resultado;
 
 //========================================================================
 //  (2) What time does the sun set in Montreal (based on the Google geocode coordinates)?
 
 const answerElement_apimashup_2 = document.getElementById('apimashup-2')
+var baseSunsetAPI = 'https://api.sunrise-sunset.org/json?';
+var lat = 'lat='+latitude
+var long = 'lng='+longitude
 
+var sunsetAPI = baseSunsetAPI+lat+'&'+long;
 
-
-
-
+request
+    .get(sunsetAPI)
+    .then(function(response){
+      var sunsetHour = response.body.results.sunset;
+      answerElement_apimashup_2.textContent = sunsetHour;
+    });
 
 //========================================================================
 //  (3) What is the weekly weather forecast in Montreal? (look for summary property in 'daily')
 
 const answerElement_apimashup_3 = document.getElementById('apimashup-3')
+var watherAPI = 'https://api.darksky.net/forecast/'
+var keyWather = '2f951a609b541b2049d0bf23f70143d3/'
+var latWather = latitude +','
+var longWhater = longitude
 
+var completeWhaterAPI = watherAPI+keyWather+latWather+longWhater;
+var test = 'https://api.darksky.net/forecast/2f951a609b541b2049d0bf23f70143d3/45.5016889,-73.567256'
 
+request
+    .get(test)
+    .then(function(response){
+      var days = response.body.daily.data;
+      days.forEach(function(day){
+        var p = document.createElement('p');
+        var hr = document.createElement('hr');
 
+        p.textContent = day.summary;
+
+        answerElement_apimashup_3.appendChild(p);
+        answerElement_apimashup_3.appendChild(hr);
+      })
+    });
+  });
 
 //
